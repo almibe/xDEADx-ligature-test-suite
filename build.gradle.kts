1653d94073aefe6b5f546a3fa74e7446082307a7
Alex Michael Berry <berry3@indiana.edu>
2020-06-13 23:57:42 -0400

    updating deps + package + org

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,6 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-    implementation("org.libraryweasel.ligature:ligature:0.1.0-SNAPSHOT")
+    implementation("dev.ligature:ligature:0.1.0-SNAPSHOT")
     implementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
