a9c112d997aaceb7848848f66cdb6c0f2b001c3b
Alex Michael Berry <alexmiberry@gmail.com>
2020-08-19 01:19:09 -0400

    rewriting to use Resource[IO, LigatureSession]

--- src/main/scala/dev/ligature/test/LigatureSuite.scala
+++ src/main/scala/dev/ligature/test/LigatureSuite.scala
@@ -4,98 +4,102 @@
 
 package dev.ligature.test
 
-import cats.implicits._
+import cats.effect.{IO, Resource}
 import dev.ligature._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 abstract class LigatureSuite extends AnyFlatSpec with Matchers {
-  def createSession(): LigatureSession
+  def createSession(): Resource[IO, LigatureSession]
 
   val testCollection: NamedEntity = NamedEntity("test")
 
   it should "Create and close store" in {
-    val store = createSession()
-    val c = store.compute.use { tx => {
-      tx.collections
-    } }.unsafeRunSync()
-    c.toSet shouldBe Set()
+    createSession().use { store => IO {
+      val c = store.compute.use { tx => {
+        tx.collections
+      } }.unsafeRunSync()
+      c.toSet shouldBe Set()
+    }}.unsafeRunSync()
   }
 
   it should "creating a new collection" in {
-    val store = createSession()
+    createSession().use { store => IO {
+      store.write.use { tx =>
+        tx.createCollection(testCollection)
+      }.unsafeRunSync()
 
-    store.write.use { tx =>
-      tx.createCollection(testCollection)
-    }.unsafeRunSync()
+      val c = store.compute.use { tx =>
+        tx.collections
+      }.unsafeRunSync()
 
-    val c = store.compute.use { tx =>
-      tx.collections
-    }.unsafeRunSync()
-
-    c.toSet shouldBe Set(testCollection)
+      c.toSet shouldBe Set(testCollection)
+    }}.unsafeRunSync()
   }
 
   it should "access and delete new collection" in {
-    val store = createSession()
+    createSession().use { store => IO {
+      store.write.use(tx => for {
+        x <- tx.createCollection(testCollection)
+      } yield x).unsafeRunSync()
 
-    store.write.use( tx => for {
-      x <- tx.createCollection(testCollection)
-    } yield x).unsafeRunSync()
+      val c = store.compute.use(tx => for {
+        c <- tx.collections
+      } yield c)
 
-    val c = store.compute.use( tx => for {
-      c <- tx.collections
-    } yield c)
+      c.unsafeRunSync().toSet shouldBe Set(testCollection)
 
-    c.unsafeRunSync().toSet shouldBe Set(testCollection)
+      store.write.use(tx => for {
+        x <- tx.deleteCollection(testCollection)
+        _ <- tx.deleteCollection(NamedEntity("Test2"))
+      } yield x).unsafeRunSync()
 
-    store.write.use( tx => for {
-      x <- tx.deleteCollection(testCollection)
-      _ <- tx.deleteCollection(NamedEntity("Test2"))
-    } yield x).unsafeRunSync()
+      val c2 = store.compute.use(tx => for {
+        c <- tx.collections
+      } yield c)
 
-    val c2 = store.compute.use( tx => for {
-      c <- tx.collections
-    } yield c)
-
-    c2.unsafeRunSync().toSet shouldBe Set()
+      c2.unsafeRunSync().toSet shouldBe Set()
+    }}.unsafeRunSync()
   }
 
   it should "new collections should be empty" in {
-    val store = createSession()
+    createSession().use { store =>
+      IO {
+        store.write.use(tx => for {
+          x <- tx.createCollection(testCollection)
+        } yield x).unsafeRunSync()
 
-    store.write.use( tx => for {
-      x <- tx.createCollection(testCollection)
-    } yield x).unsafeRunSync()
+        val s = store.compute.use(tx => for {
+          s <- tx.allStatements(testCollection)
+        } yield s)
 
-    val s = store.compute.use( tx => for {
-      s <- tx.allStatements(testCollection)
-    } yield s)
-
-    s.unsafeRunSync().toSet shouldBe Set()
-
+        s.unsafeRunSync().toSet shouldBe Set()
+      }
+    }.unsafeRunSync()
   }
 
   it should "adding statements to collections" in {
-    val store = createSession()
+    createSession().use { store =>
+      IO {
+        store.write.use(tx => for {
+          _ <- tx.addStatement(testCollection, Statement(NamedEntity("Alex"), Ligature.a, NamedEntity("Human")))
+          r <- tx.addStatement(testCollection, Statement(NamedEntity("Clarice"), Ligature.a, NamedEntity("Feline")))
+        } yield r).unsafeRunSync()
 
-    store.write.use( tx => for {
-      _ <- tx.addStatement(testCollection, Statement(NamedEntity("Alex"), Ligature.a, NamedEntity("Human")))
-      r <- tx.addStatement(testCollection, Statement(NamedEntity("Clarice"), Ligature.a, NamedEntity("Feline")))
-    } yield r).unsafeRunSync()
+        val s = store.compute.use(tx => for {
+          s <- tx.allStatements(testCollection)
+        } yield s)
 
-    val s = store.compute.use( tx => for {
-      s <- tx.allStatements(testCollection)
-    } yield s)
-
-    s.unsafeRunSync().map((ps: PersistedStatement) => ps.statement).toSet shouldBe
-      Set(Statement(NamedEntity("Alex"), Ligature.a, NamedEntity("Human")),
+        s.unsafeRunSync().map((ps: PersistedStatement) => ps.statement).toSet shouldBe
+          Set(Statement(NamedEntity("Alex"), Ligature.a, NamedEntity("Human")),
             Statement(NamedEntity("Clarice"), Ligature.a, NamedEntity("Feline")))
 
+      }
+    }.unsafeRunSync()
   }
 
 //  it should "removing statements from collections" in {
-//    val store = createSession()
+//    createSession().use { store => IO {
 //
 //    store.write.use( tx => for {
 //      _ <- tx.addStatement(testCollection, Statement(NamedEntity("Alex"), Ligature.a, NamedEntity("Human")))
@@ -113,29 +117,31 @@ abstract class LigatureSuite extends AnyFlatSpec with Matchers {
 //  }
 
   it should "new entity test" in {
-    val store = createSession()
+    createSession().use { store =>
+      IO {
 
-    store.write.use( tx => for {
-      e0 <- tx.newEntity(testCollection)
-      e1 <- tx.newEntity(testCollection)
-      e2 <- tx.newEntity(testCollection)
-      e3 <- tx.newEntity(testCollection)
-      _ <- tx.addStatement(testCollection, Statement(e0.get, Ligature.a, e1.get))
-      _ <- tx.addStatement(testCollection, Statement(e2.get, Ligature.a, e3.get))
-    } yield ()).unsafeRunSync()
+        store.write.use(tx => for {
+          e0 <- tx.newEntity(testCollection)
+          e1 <- tx.newEntity(testCollection)
+          e2 <- tx.newEntity(testCollection)
+          e3 <- tx.newEntity(testCollection)
+          _ <- tx.addStatement(testCollection, Statement(e0.get, Ligature.a, e1.get))
+          _ <- tx.addStatement(testCollection, Statement(e2.get, Ligature.a, e3.get))
+        } yield ()).unsafeRunSync()
 
-    val s = store.compute.use( tx => for {
-      s <- tx.allStatements(testCollection)
-    } yield s)
+        val s = store.compute.use(tx => for {
+          s <- tx.allStatements(testCollection)
+        } yield s)
 
-    s.unsafeRunSync().map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
-      Statement(AnonymousEntity(0), Ligature.a, AnonymousEntity(1)),
-      Statement(AnonymousEntity(2), Ligature.a, AnonymousEntity(3)))
-
+        s.unsafeRunSync().map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
+          Statement(AnonymousEntity(0), Ligature.a, AnonymousEntity(1)),
+          Statement(AnonymousEntity(2), Ligature.a, AnonymousEntity(3)))
+      }
+    }.unsafeRunSync()
   }
 
 //  it should "removing named entity" in {
-//    val store = createSession()
+//    createSession().use { store => IO {
 //    val entA = NamedEntity("a")
 //    val entB = NamedEntity("b")
 //    val entC = NamedEntity("c")
@@ -157,7 +163,7 @@ abstract class LigatureSuite extends AnyFlatSpec with Matchers {
 //  }
 //
 //  it should "removing anonymous entity" in {
-//    val store = createSession()
+//    createSession().use { store => IO {
 //
 //    store.write.use( tx => for {
 //      ent1 <- tx.newEntity(testCollection)
@@ -179,7 +185,7 @@ abstract class LigatureSuite extends AnyFlatSpec with Matchers {
 //  }
 //
 //  it should "removing predicate" in {
-//    val store = createSession()
+//    createSession().use { store => IO {
 //    val namedA = NamedEntity(Ligature.a.identifier)
 //    store.write.use( tx => for {
 //      ent1 <- tx.newEntity(testCollection)
@@ -199,57 +205,62 @@ abstract class LigatureSuite extends AnyFlatSpec with Matchers {
 //  }
 
   it should "matching against a non-existant collection" in {
-    val store = createSession()
+    createSession().use { store =>
+      IO {
+        val s = store.compute.use(tx => for {
+          s <- tx.matchStatements(testCollection, None, None, Some(StringLiteral("French")))
+          s2 <- tx.matchStatements(testCollection, None, Some(Ligature.a), None)
+        } yield (s, s2))
 
-    val s = store.compute.use( tx => for {
-      s <- tx.matchStatements(testCollection, None, None, Some(StringLiteral("French")))
-      s2 <- tx.matchStatements(testCollection, None, Some(Ligature.a), None)
-    } yield (s, s2))
+        s.unsafeRunSync()._1.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set()
+        s.unsafeRunSync()._2.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set()
 
-    s.unsafeRunSync()._1.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set()
-    s.unsafeRunSync()._2.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set()
-
+      }
+    }.unsafeRunSync()
   }
 
   it should "matching statements in collections" in {
-      val store = createSession()
-      val valjean = NamedEntity("valjean")
-      val javert = NamedEntity("javert")
+    createSession().use { store =>
+      IO {
+        val valjean = NamedEntity("valjean")
+        val javert = NamedEntity("javert")
 
-      store.write.use( tx => for {
-        _ <- tx.addStatement(testCollection, Statement(valjean, Predicate("nationality"), StringLiteral("French")))
-        _ <- tx.addStatement(testCollection, Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)))
-        _ <- tx.addStatement(testCollection, Statement(javert, Predicate("nationality"), StringLiteral("French")))
-      } yield ()).unsafeRunSync()
+        store.write.use(tx => for {
+          _ <- tx.addStatement(testCollection, Statement(valjean, Predicate("nationality"), StringLiteral("French")))
+          _ <- tx.addStatement(testCollection, Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)))
+          _ <- tx.addStatement(testCollection, Statement(javert, Predicate("nationality"), StringLiteral("French")))
+        } yield ()).unsafeRunSync()
 
-    val s = store.compute.use( tx => for {
-      s <- tx.matchStatements(testCollection, None, None, Some(StringLiteral("French")))
-      s2 <- tx.matchStatements(testCollection, None, None, Some(LongLiteral(24601)))
-      s3 <- tx.matchStatements(testCollection, Some(valjean))
-      s4 <- tx.matchStatements(testCollection, Some(javert),
-        Some(Predicate("nationality")),
-        Some(StringLiteral("French")))
-      s5 <- tx.matchStatements(testCollection, None, None, None)
-    } yield (s, s2, s3, s4, s5))
+        val s = store.compute.use(tx => for {
+          s <- tx.matchStatements(testCollection, None, None, Some(StringLiteral("French")))
+          s2 <- tx.matchStatements(testCollection, None, None, Some(LongLiteral(24601)))
+          s3 <- tx.matchStatements(testCollection, Some(valjean))
+          s4 <- tx.matchStatements(testCollection, Some(javert),
+            Some(Predicate("nationality")),
+            Some(StringLiteral("French")))
+          s5 <- tx.matchStatements(testCollection, None, None, None)
+        } yield (s, s2, s3, s4, s5))
 
-    s.unsafeRunSync()._1.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
-      Statement(valjean, Predicate("nationality"), StringLiteral("French")),
-      Statement(javert, Predicate("nationality"), StringLiteral("French")))
-    s.unsafeRunSync()._2.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
-      Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)))
-    s.unsafeRunSync()._3.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
-      Statement(valjean, Predicate("nationality"), StringLiteral("French")),
-      Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)))
-    s.unsafeRunSync()._4.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
-      Statement(javert, Predicate("nationality"), StringLiteral("French")))
-    s.unsafeRunSync()._5.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
-      Statement(valjean, Predicate("nationality"), StringLiteral("French")),
-      Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)),
-      Statement(javert, Predicate("nationality"), StringLiteral("French")))
+        s.unsafeRunSync()._1.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
+          Statement(valjean, Predicate("nationality"), StringLiteral("French")),
+          Statement(javert, Predicate("nationality"), StringLiteral("French")))
+        s.unsafeRunSync()._2.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
+          Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)))
+        s.unsafeRunSync()._3.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
+          Statement(valjean, Predicate("nationality"), StringLiteral("French")),
+          Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)))
+        s.unsafeRunSync()._4.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
+          Statement(javert, Predicate("nationality"), StringLiteral("French")))
+        s.unsafeRunSync()._5.map((ps: PersistedStatement) => ps.statement).toSet shouldBe Set(
+          Statement(valjean, Predicate("nationality"), StringLiteral("French")),
+          Statement(valjean, Predicate("prisonNumber"), LongLiteral(24601)),
+          Statement(javert, Predicate("nationality"), StringLiteral("French")))
+      }
+    }.unsafeRunSync()
   }
 
 //  it should "matching statements with literals and ranges in collections" in {
-//    val store = createSession()
+//    createSession().use { store => IO {
 //    val valjean = NamedEntity("valjean")
 //    val javert = NamedEntity("javert")
 //    val trout = NamedEntity("trout")
